511dff8e718681b636a08d860b2e7e0882531f69
learnmore147 <63195075+learnmore147@users.noreply.github.com>
2020-04-18 00:42:28 -1000

    Update vpnsetup_centos.sh

--- vpnsetup_centos.sh
+++ vpnsetup_centos.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK='sanchezjarich'
-YOUR_USERNAME='sanchezjarich'
-YOUR_PASSWORD='sanchezjarich'
+YOUR_IPSEC_PSK=''
+YOUR_USERNAME=''
+YOUR_PASSWORD=''
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
